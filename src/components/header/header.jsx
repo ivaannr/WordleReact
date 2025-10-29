bbe6508502d01e44da16e87f31c5238b8a3a77ea
maquin virtual <romuvan13@gmail.com>
2025-10-29 12:55:33 +0100

    header added

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './header.css'
+
+export default function Header() {
+    return (
+        <div id="header">
+            <h1>Wordle</h1>
+            <hr/>
+        </div>
+    );
+}
