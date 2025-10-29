c9b51f25e5ceb334b6eb7be07692526702f1e40a
maquin virtual <romuvan13@gmail.com>
2025-10-29 13:14:54 +0100

    maincontainer component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
+import MainContainer from './components/main-container/main-container'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header />
+      <MainContainer />
       <Footer />
     </>
   )
---
+++ src/components/main-container/main-container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './main-container.css'
+
+export default function MainContainer() {
+    return (
+        <>
+            <div className="box">
+            </div>
+        </>
+    );
+}
