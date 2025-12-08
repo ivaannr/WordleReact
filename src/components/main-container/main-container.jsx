e74da2036f9cd350c853ebd188f3d46553bf46ce
ivaannr <romuvan13@gmail.com>
2025-12-08 12:39:59 +0100

    first commit

--- src/components/main-container/main-container.jsx
+++ src/components/main-container/main-container.jsx
@@ -1,9 +1,9 @@
 import './main-container.css'
 import Word from '../word/word';
 import { useEffect } from 'react';
-export default function MainContainer(props) {
-  const { currentWordIndex, currentLetterIndex, length, lettersData, previousWords, currentLetter, previousLetters, setPreviousLetters } = props
 
+export default function MainContainer( { currentWordIndex, currentLetterIndex, length, lettersData, previousWords, currentLetter, previousLetters, setPreviousLetters } ) {
+  
   return (
     <div className="box">
       {Array.from({ length: 6 }).map((_, index) => {
